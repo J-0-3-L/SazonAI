ec3d29f34957185ad0f3c0acbd8d093f5cc7035e
Joel Lawi <abecedegluu@gmail.com>
2025-02-13 21:08:04 -0500

    Add: Integrando flask-restx

--- backend/sazonAI/__init__.py
+++ backend/sazonAI/__init__.py
@@ -1,10 +1,25 @@
 from flask import Flask
+from flask_restx import Api
 
+
+api = Api()
 
 def create_app():
 
     app = Flask(__name__)
 
+    api = Api(
+        app,
+        title='SazonAI',
+        version='1.0',
+        description='SazonAI es un recetarios personalizado que te brinda recetas de acuerdo a las recetas buscadas haciendo uso de una AI',
+        doc='/docs'
+    )
+
+    with app.app_context():
+        
+        from sazonAI.recipes.routes import api as recipes_api
+        api.add_namespace(recipes_api)
 
     return app
 
