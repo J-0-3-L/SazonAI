886d1c283109dad747e31706fce6c6a21f5336a3
Joel Lawi <abecedegluu@gmail.com>
2025-02-15 21:15:52 -0500

    Add: Work route post

--- backend/sazonAI/recipes/routes.py
+++ backend/sazonAI/recipes/routes.py
@@ -1,4 +1,5 @@
 from flask_restx import Resource, Namespace
+from flask import request
 
 api = Namespace('recipes',description='Operacion de recetas')
 
@@ -6,8 +7,13 @@ api = Namespace('recipes',description='Operacion de recetas')
 class RecipesResource(Resource):
     
     def post(self):
+        ingredients = request.get_json()
+        ingredients_str = ", ".join(ingredients)
 
-        pass
+        prompt = f"Generame recetas con estos ingredientes: {ingredients_str}"
 
+        response = 
+        
+    
     def get(self):
         pass
