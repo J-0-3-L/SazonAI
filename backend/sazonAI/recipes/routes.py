e485b9c29afb2d3485b6037eb714b10fc959b7b4
Joel Lawi <abecedegluu@gmail.com>
2025-02-17 21:26:44 -0500

    Add: Work route post p2

--- backend/sazonAI/recipes/routes.py
+++ backend/sazonAI/recipes/routes.py
@@ -1,7 +1,10 @@
 from flask_restx import Resource, Namespace
 from flask import request
+from google import genai
 
 api = Namespace('recipes',description='Operacion de recetas')
+
+client = genai.Client(api_key='YOUR_API_KEY')
 
 @api.route('/')
 class RecipesResource(Resource):
@@ -12,7 +15,12 @@ class RecipesResource(Resource):
 
         prompt = f"Generame recetas con estos ingredientes: {ingredients_str}"
 
-        response = 
+        response = client.models.generate_content(
+            model = 'gemini-2.0-flash',
+            contents = prompt
+        )
+
+        return response
         
     
     def get(self):
