b4b7b6c053027bf9182800e1d7eb8450a9e55972
Joel Lawi <abecedegluu@gmail.com>
2025-02-18 18:16:17 -0500

    Add: Work route post p3

--- backend/sazonAI/recipes/routes.py
+++ backend/sazonAI/recipes/routes.py
@@ -6,21 +6,38 @@ api = Namespace('recipes',description='Operacion de recetas')
 
 client = genai.Client(api_key='YOUR_API_KEY')
 
+generate_recipes = []
+
 @api.route('/')
 class RecipesResource(Resource):
     
     def post(self):
         ingredients = request.get_json()
-        ingredients_str = ", ".join(ingredients)
+
+        ingredients_limit = ingredients[:10]
+        ingredients_str = ", ".join(ingredients_limit)
 
         prompt = f"Generame recetas con estos ingredientes: {ingredients_str}"
 
         response = client.models.generate_content(
             model = 'gemini-2.0-flash',
-            contents = prompt
+            contents = prompt,
+            max_tokens = 150
         )
 
-        return response
+        recipes = response['choices'][0]['text']
+        recipe_list = recipes.split("\n")
+
+        clean_recipes = []
+
+        for recipe in recipe_list:
+            recipe_name = recipe.strip()
+
+            clean_recipes.append(recipe_name)
+
+        generate_recipes.append(clean_recipes)
+
+        return {'recipes':clean_recipes}, 201
         
     
     def get(self):
