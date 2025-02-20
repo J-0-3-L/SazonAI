9c4c830a5e4443a7e9c34d56a62d99b94ac63fbf
Joel Lawi <abecedegluu@gmail.com>
2025-02-20 18:47:24 -0500

    Add: Work route post p6

--- backend/sazonAI/recipes/routes.py
+++ backend/sazonAI/recipes/routes.py
@@ -1,14 +1,28 @@
 from flask_restx import Resource, Namespace, fields
 from flask import request
+from dotenv import load_dotenv
+import os
 from google import genai
+
+load_dotenv()
 
 api = Namespace('recipes',description='Operacion de recetas')
 
 recipe_model = api.model('Recipe',{
-    'name': fields.String(required=True)
+    'ingredient': fields.String(required=True),
+    'ingredients': fields.List(fields.String, required=True)
 })
 
-client = genai.Client(api_key='YOUR_API_KEY')
+
+recipe_response_model = api.model('RecipeResponse', {
+    'recipes': fields.List(fields.Nested(api.model('SingleRecipe', {
+        'recipe': fields.String(required=True)
+    })))
+})
+
+api_key = os.getenv('YOUR_API_KEY')
+
+client = genai.Client(api_key=api_key)
 
 generate_recipes = []
 
@@ -16,48 +30,55 @@ generate_recipes = []
 class RecipesResource(Resource):
 
 
-    @api.marshal_with(recipe_model)
+    @api.marshal_with(recipe_response_model)
     @api.expect(recipe_model)
     def post(self):
-        ingredients = request.get_json()
-
-        if not ingredients or len(ingredients)==0:
-            return {'message': 'Ingrese el ingrediente'}, 400
-
-        if isinstance(ingredients, str):
-            ingredients = [ingredients]
-
-        if isinstance(ingredients, list):
-            ingredients_limit = ingredients[:10]
+               
+        data = request.get_json()
+        if isinstance(data, dict):
+            if 'ingredients' in data:
+                ingredients = data['ingredients']
+            elif 'ingredient' in data:
+                ingredients = [data['ingredient']]
+            else:
+                return {'message': 'Debe enviar al menos un ingrediente'}, 400
+        elif isinstance(data, str):
+            ingredients = [data]
         else:
-            return {'message':'Los ingredientes deben ser una lista o una cadena de texto'}
+            return {'message': 'Formato de datos incorrecto'}, 400
 
-        #ingredients_limit = ingredients[:10]
+        ingredients_limit = ingredients[:10]
         ingredients_str = ", ".join(ingredients_limit)
 
-        prompt = f"Generame recetas con estos ingredientes: {ingredients_str}"
+        prompt = f"Genera 10 recetas mencionando solo el nombre,sin encabezados, sin enumeraciones , sin descripciones, detalles ni sugerencias, con los siguientes ingredientes: {ingredients_str}"
 
         response = client.models.generate_content(
             model = 'gemini-2.0-flash',
             contents = prompt,
-            max_tokens = 150
+            #max_output_tokens = 150
         )
 
-        recipes = response['choices'][0]['text']
+        #print(response)
+
+        recipes = response.text
         recipe_list = recipes.split("\n")
-        print(recipe_list)
+        #print(recipe_list)
 
         clean_recipes = []
 
         for recipe in recipe_list:
             recipe_name = recipe.strip()
+
+            if recipe_name.startswith("*"):
+                recipe_name = recipe_name.lstrip("*").strip()
+
             if recipe_name:
-                clean_recipes.append({'name':recipe_name})
+                clean_recipes.append({'recipe':recipe_name})
 
         #print(clean_recipes)    
         generate_recipes.append(clean_recipes)
 
-        return {'recipes':clean_recipes}, 201
+        return 'ok', 201
         
     
     def get(self):
