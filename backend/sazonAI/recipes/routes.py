0d1f6d9752d264bbcbe60e6f58a0f2715738f275
Joel Lawi <abecedegluu@gmail.com>
2025-02-19 20:50:21 -0500

    Add: Work route post p5

--- backend/sazonAI/recipes/routes.py
+++ backend/sazonAI/recipes/routes.py
@@ -1,8 +1,12 @@
-from flask_restx import Resource, Namespace
+from flask_restx import Resource, Namespace, fields
 from flask import request
 from google import genai
 
 api = Namespace('recipes',description='Operacion de recetas')
+
+recipe_model = api.model('Recipe',{
+    'name': fields.String(required=True)
+})
 
 client = genai.Client(api_key='YOUR_API_KEY')
 
@@ -10,14 +14,25 @@ generate_recipes = []
 
 @api.route('/')
 class RecipesResource(Resource):
-    
+
+
+    @api.marshal_with(recipe_model)
+    @api.expect(recipe_model)
     def post(self):
         ingredients = request.get_json()
 
         if not ingredients or len(ingredients)==0:
             return {'message': 'Ingrese el ingrediente'}, 400
 
-        ingredients_limit = ingredients[:10]
+        if isinstance(ingredients, str):
+            ingredients = [ingredients]
+
+        if isinstance(ingredients, list):
+            ingredients_limit = ingredients[:10]
+        else:
+            return {'message':'Los ingredientes deben ser una lista o una cadena de texto'}
+
+        #ingredients_limit = ingredients[:10]
         ingredients_str = ", ".join(ingredients_limit)
 
         prompt = f"Generame recetas con estos ingredientes: {ingredients_str}"
@@ -30,14 +45,16 @@ class RecipesResource(Resource):
 
         recipes = response['choices'][0]['text']
         recipe_list = recipes.split("\n")
+        print(recipe_list)
 
         clean_recipes = []
 
         for recipe in recipe_list:
             recipe_name = recipe.strip()
+            if recipe_name:
+                clean_recipes.append({'name':recipe_name})
 
-            clean_recipes.append(recipe_name)
-
+        #print(clean_recipes)    
         generate_recipes.append(clean_recipes)
 
         return {'recipes':clean_recipes}, 201
