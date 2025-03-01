1fa03ce85e07a7a38bbf0c05e555e1abfb0cf57a
Joel Lawi <abecedegluu@gmail.com>
2025-02-28 21:31:32 -0500

    Add: Work route results recipes

--- backend/sazonAI/recipes/results/routes.py
+++ backend/sazonAI/recipes/results/routes.py
@@ -1,1 +1,18 @@
-ECHO est  activado.
+from flask_restx import Namespace, fields, Resource
+from flask import request
+from dotenv import load_dotenv
+import os
+from google import genai
+
+load_dotenv()
+
+@api.route('/InfoRecipes')
+class InfoRecipe(Resource):
+    
+    def post(self):
+        pass
+
+
+    def get(self):
+        pass
+
