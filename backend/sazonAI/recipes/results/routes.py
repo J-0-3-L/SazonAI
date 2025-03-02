d414521466f2572b62d347b3d726c62aebaeb072
Joel Lawi <abecedegluu@gmail.com>
2025-03-01 19:03:49 -0500

    Add: Work route results recipes p2

--- backend/sazonAI/recipes/results/routes.py
+++ backend/sazonAI/recipes/results/routes.py
@@ -6,10 +6,23 @@ from google import genai
 
 load_dotenv()
 
+api_key=os.getenv('YOUR_API_KEY')
+
+client = genai.Client(api_key=api_key)
+
 @api.route('/InfoRecipes')
 class InfoRecipe(Resource):
     
     def post(self):
+
+        recipe_name = request.json.get('recipe_name')
+        prompt = f"Genera una receta con título, descripción, ingredientes y preparación para la receta: {recipe_name}"
+
+        response = client.models.generate_content(
+            model='gemini-2.0-flash',
+            contents=prompt
+        )
+
         pass
 
 
